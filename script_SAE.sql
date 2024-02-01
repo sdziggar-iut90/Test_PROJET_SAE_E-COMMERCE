5008fb6eb53edf44e3d4e52c9bdfb59c4fb3862a
aorngom <144449285+aorngom@users.noreply.github.com>
2024-02-01 17:20:52 +0100

    Add files via upload

---
+++ script_SAE.sql
@@ -0,0 +1,26 @@
+DROP TABLE utilisateur;
+
+CREATE TABLE utilisateur(
+    id_utilisateur INT AUTO_INCREMENT,
+    login VARCHAR(55),
+    email VARCHAR (255),
+    nom VARCHAR(25),
+    password VARCHAR(255),
+    role VARCHAR(25),
+    est_actif tinyint(1),
+    PRIMARY KEY(id_utilisateur)
+)ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
+
+
+INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
+(1,'admin','admin@admin.fr',
+    'sha256$dPL3oH9ug1wjJqva$2b341da75a4257607c841eb0dbbacb76e780f4015f0499bb1a164de2a893fdbf',
+    'ROLE_admin','admin','1'),
+(2,'client','client@client.fr',
+    'sha256$1GAmexw1DkXqlTKK$31d359e9adeea1154f24491edaa55000ee248f290b49b7420ced542c1bf4cf7d',
+    59'ROLE_client','client','1'),
+(3,'client2','client2@client2.fr',
+    'sha256$MjhdGuDELhI82lKY$2161be4a68a9f236a27781a7f981a531d11fdc50e4112d912a7754de2dfa0422',
+    'ROLE_client','client2','1');
+
+    
