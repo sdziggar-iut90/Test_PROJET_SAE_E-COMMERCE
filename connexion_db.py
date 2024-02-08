aecd069c3a5fa9b4eeadc16b28e06ea3071bc3a2
aorngom <144449285+aorngom@users.noreply.github.com>
2024-02-08 11:27:46 +0100

    Update connexion_db.py

--- connexion_db.py
+++ connexion_db.py
@@ -11,7 +11,7 @@ def get_db():
             # host="serveurmysql",
             user="aorngom",
             password="Tawoukeurgui@2009",
-            database="BDD_Base_De_Données",
+            database="BDD_Base_De_Donnees",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
